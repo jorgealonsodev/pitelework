05520482ae5059388fffddbd13de05e686282e43
Jorge Alonso <jorgealonsodev@gmail.com>
2020-11-23 13:50:03 +0000

    Modificado docker-compose y añadida carpeta nginx-ssl

---
+++ docker/docker-letsencrypt-nginx-proxy-companion-master/app/start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# SIGTERM-handler
+term_handler() {
+    [[ -n "$docker_gen_pid" ]] && kill "$docker_gen_pid"
+    [[ -n "$letsencrypt_service_pid" ]] && kill "$letsencrypt_service_pid"
+
+    # shellcheck source=functions.sh
+    source /app/functions.sh
+    remove_all_location_configurations
+    remove_all_standalone_configurations
+
+    exit 0
+}
+
+trap 'term_handler' INT QUIT TERM
+
+/app/letsencrypt_service &
+letsencrypt_service_pid=$!
+
+wait_default="500ms:2s"
+DOCKER_GEN_WAIT="${DOCKER_GEN_WAIT:-$wait_default}"
+docker-gen -watch -notify '/app/signal_le_service' -wait "$DOCKER_GEN_WAIT" /app/letsencrypt_service_data.tmpl /app/letsencrypt_service_data &
+docker_gen_pid=$!
+
+# wait "indefinitely"
+while [[ -e /proc/$docker_gen_pid ]]; do
+    wait $docker_gen_pid # Wait for any signals or end of execution of docker-gen
+done
+
+# Stop container properly
+term_handler
---
+++ docker/docker-letsencrypt-nginx-proxy-companion-master/test/config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+testAlias+=(
+	[jrcs/letsencrypt-nginx-proxy-companion]='le-companion'
+)
+
+imageTests+=(
+	[le-companion]='
+	docker_api
+	location_config
+	default_cert
+	certs_single
+	certs_san
+	certs_single_domain
+	certs_standalone
+	force_renew
+	certs_validity
+	container_restart
+	permissions_default
+	permissions_custom
+	symlinks
+	'
+)
